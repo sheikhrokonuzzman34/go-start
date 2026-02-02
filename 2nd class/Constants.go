b681fb928d80672b742682849e78f3ee9772966d
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-02-02 15:54:54 +0600

    const pi create

---
+++ 2nd class/Constants.go
@@ -0,0 +1,20 @@
+// package main
+// import ("fmt")
+
+// const A = false
+
+// func main() {
+//   fmt.Println(A)
+//   fmt.Printf("%T\n", A)
+// }
+
+
+package main
+import ("fmt")
+
+const PI = 3.14
+
+func main() {
+  fmt.Println(PI)
+  fmt.Printf("%T\n", PI)
+}
