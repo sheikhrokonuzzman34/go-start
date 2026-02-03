5b77ebfe7283138a08a361587cfb2a8c4c336a44
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-02-03 15:41:10 +0600

    for loop create

--- class_3th/if_else_statement.go
+++ class_3th/if_else_statement.go
@@ -1,5 +1,5 @@
-package main
-import ("fmt")
+// package main
+// import ("fmt")
 
 // func main() {
 //   time := 20
@@ -35,13 +35,32 @@ import ("fmt")
 //   }
 // }
 
+// func main() {
+//   x := 30
+//   if x >= 10 {
+//     fmt.Println("x is larger than or equal to 10.")
+//   } else if x > 20 {
+//     fmt.Println("x is larger than 20.")
+//   } else {
+//     fmt.Println("x is less than 10.")
+//   }
+// }
+
+
+package main
+import ("fmt")
+
 func main() {
-  x := 30
-  if x >= 10 {
-    fmt.Println("x is larger than or equal to 10.")
-  } else if x > 20 {
-    fmt.Println("x is larger than 20.")
-  } else {
-    fmt.Println("x is less than 10.")
+   day := 5
+
+   switch day {
+   case 1,3,5:
+    fmt.Println("Odd weekday")
+   case 2,4:
+     fmt.Println("Even weekday")
+   case 6,7:
+    fmt.Println("Weekend")
+  default:
+    fmt.Println("Invalid day of day number")
   }
 }
