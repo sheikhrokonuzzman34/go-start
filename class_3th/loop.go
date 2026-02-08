9fdcfa22c36d126c22ccebbed41afac8594e8e99
sheikhrokonuzzman34 <mdrokon.u545@gmail.com>
2026-02-08 12:08:22 +0600

    logp

--- class_3th/loop.go
+++ class_3th/loop.go
@@ -1,10 +1,24 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println("Iteration number:", i)
+// 	}
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	array := [3]int{10, 20, 30}
 
-	for i := 0; i < 5; i++ {
-		fmt.Println("Iteration number:", i)
-	}
+	fo
+
+
 }
